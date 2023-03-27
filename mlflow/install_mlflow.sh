715cfe9868f5961c0056d598a381039366623158
Muntasir2001 <muntasirenamalhuda@gmail.com>
2023-03-27 16:59:12 +0100

    working on creating first model

--- mlflow/install_mlflow.sh
+++ mlflow/install_mlflow.sh
@@ -1,3 +1,3 @@
 echo $TEST
 
-mlflow server --backend-store-uri=sqlite:///mlflow-gridicity.db --default-artifact-root=file:mlruns --host 0.0.0.0 --port 5000
+mlflow server --backend-store-uri=sqlite:///mlflow-gridicity.db --default-artifact-root=s3://test_bucket --host 0.0.0.0 --port 5000
